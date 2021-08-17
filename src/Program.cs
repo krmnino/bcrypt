ea95a1dde897f69bb219425e3ec2462943594dcb
krmnino <kurt.manrique.n@gmail.com>
2021-08-17 00:56:20 -0400

    implemented constructor that accepts salt in base64 encoding and cleaned up code

--- src/Program.cs
+++ src/Program.cs
@@ -6,7 +6,8 @@ namespace Bcrypt
     {
         static void Main(string[] args)
         {
-            Bcrypt b = new Bcrypt("<.S.2K(Zq'", 4);
+            String cleartext = "<.S.2K(Zq'";
+            Bcrypt b = new Bcrypt(ref cleartext, 4);
             String ret = b.Hash;
             Console.WriteLine(ret);
 
@@ -18,8 +19,15 @@ namespace Bcrypt
             enc = empty.EncodeBase64(ref dec, 0);
             Console.WriteLine(enc);
 
-            Bcrypt custom_salt = new Bcrypt("<.S.2K(Zq'", ref dec, 8);
+            cleartext = "<.S.2K(Zq'";
+            Bcrypt custom_salt = new Bcrypt(ref cleartext, ref dec, 8);
             ret = custom_salt.Hash;
+            Console.WriteLine(ret);
+
+            cleartext = "<.S.2K(Zq'";
+            base64_salt = "VYAclAMpaXY/oqAo9yUpku";
+            Bcrypt custom_b64salt = new Bcrypt(ref cleartext, ref base64_salt, 8);
+            ret = custom_b64salt.Hash;
             Console.WriteLine(ret);
         }
     }
