d388ecdef86884145d35f8047c0d8211a248424c
krmnino <kurt.manrique.n@gmail.com>
2022-03-08 00:37:29 -0500

    created assert(), test1() and test2()

--- src/Program.cs
+++ src/Program.cs
@@ -4,6 +4,37 @@ namespace Bcrypt
 {
     class Program
     {
+        static void assert(bool expr, int testcase)
+        {
+            if (!expr)
+            {
+                Console.WriteLine("Error: Assertion failed at testcase " + testcase + ".");
+                System.Environment.Exit(1);
+            }
+        }
+        static void test1()
+        {
+            String cleartext = "<.S.2K(Zq'";
+            String salt = "VYAclAMpaXY/oqAo9yUpku";
+            int cost = 4;
+            Bcrypt b1 = new Bcrypt(ref cleartext, ref salt, cost);
+            String expected_hash = "$2b$04$VYAclAMpaXY/oqAo9yUpkuWmoYywaPzyhu56HxXpVltnBIfmO9tgu";
+            String actual_hash = b1.Hash;
+            assert(String.Equals(expected_hash, actual_hash), 1);
+            Console.WriteLine(">> Test 1 successful");
+        }
+
+        static void test2()
+        {
+            String cleartext = "5.rApO%5jA";
+            String salt = "kVNDrnYKvbNr5AIcxNzeIu";
+            int cost = 5;
+            Bcrypt b = new Bcrypt(ref cleartext, ref salt, cost);
+            String expected_hash = "$2b$05$kVNDrnYKvbNr5AIcxNzeIuRcyIF5cZk6UrwHGxENbxP5dVv.WQM/G";
+            String actual_hash = b.Hash;
+            assert(String.Equals(expected_hash, actual_hash), 1);
+            Console.WriteLine(">> Test 2 successful");
+        }
         static void Main(string[] args)
         {
             String cleartext;
@@ -14,29 +45,11 @@ namespace Bcrypt
 
             //////////////////////////////////////////////////////////////////////////////////////////////
 
-            Console.WriteLine("Test Case #1");
-            cleartext = "<.S.2K(Zq'";
-            salt = "VYAclAMpaXY/oqAo9yUpku";
-            cost = 4;
-            Bcrypt b1 = new Bcrypt(ref cleartext, ref salt, cost);
-            expected_hash = "$2a$04$VYAclAMpaXY/oqAo9yUpkuWmoYywaPzyhu56HxXpVltnBIfmO9tgu";
-            actual_hash = b1.Hash;
-            Console.WriteLine("Expected -> " + expected_hash);
-            Console.WriteLine("Actual   -> " + actual_hash);
-            Console.WriteLine("========================================================================");
+            test1();
 
             //////////////////////////////////////////////////////////////////////////////////////////////
 
-            Console.WriteLine("Test Case #2");
-            cleartext = "5.rApO%5jA";
-            salt = "kVNDrnYKvbNr5AIcxNzeIu";
-            cost = 5;
-            Bcrypt b2 = new Bcrypt(ref cleartext, ref salt, cost);
-            expected_hash = "$2a$05$kVNDrnYKvbNr5AIcxNzeIuRcyIF5cZk6UrwHGxENbxP5dVv.WQM/G";
-            actual_hash = b2.Hash;
-            Console.WriteLine("Expected -> " + expected_hash);
-            Console.WriteLine("Actual   -> " + actual_hash);
-            Console.WriteLine("========================================================================");
+            test2();
 
             //////////////////////////////////////////////////////////////////////////////////////////////
 
