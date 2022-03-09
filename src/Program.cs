2f3ec5a31d49b1025cdce9ebdc57fd95dac8ff03
krmnino <kurt.manrique.n@gmail.com>
2022-03-09 01:31:45 -0500

    implemented test3-6()

--- src/Program.cs
+++ src/Program.cs
@@ -12,6 +12,7 @@ namespace Bcrypt
                 System.Environment.Exit(1);
             }
         }
+
         static void test1()
         {
             String cleartext = "<.S.2K(Zq'";
@@ -32,76 +33,90 @@ namespace Bcrypt
             Bcrypt b = new Bcrypt(ref cleartext, ref salt, cost);
             String expected_hash = "$2b$05$kVNDrnYKvbNr5AIcxNzeIuRcyIF5cZk6UrwHGxENbxP5dVv.WQM/G";
             String actual_hash = b.Hash;
-            assert(String.Equals(expected_hash, actual_hash), 1);
+            assert(String.Equals(expected_hash, actual_hash), 2);
             Console.WriteLine(">> Test 2 successful");
         }
-        static void Main(string[] args)
+
+        static void test3()
         {
-            String cleartext;
-            String salt;
-            int cost;
-            String expected_hash;
-            String actual_hash;
+            String cleartext = "<.S.2K(Zq'";
+            String salt = "VYAclAMpaXY/oqAo9yUpku";
+            int cost = 4;
+            Bcrypt b = new Bcrypt(ref cleartext, ref salt, cost);
+            String expected_hash = "$2b$04$VYAclAMpaXY/oqAo9yUpkuWmoYywaPzyhu56HxXpVltnBIfmO9tgu";
+            String actual_hash = b.Hash;
+            assert(String.Equals(expected_hash, actual_hash), 3);
+            Console.WriteLine(">> Test 3 successful");
+        }
 
-            //////////////////////////////////////////////////////////////////////////////////////////////
+        static void test4()
+        {
+            String cleartext = "<.S.2K(Zq'";
+            String salt = "VYAclAMpaXY/oqAo9yUpku";
+            int cost = 4;
+            Bcrypt b = new Bcrypt(ref cleartext, ref salt, cost);
+            String expected_hash = "$2b$04$VYAclAMpaXY/oqAo9yUpkuWmoYywaPzyhu56HxXpVltnBIfmO9tgu";
+            String actual_hash = b.Hash;
+            assert(String.Equals(expected_hash, actual_hash), 4);
+            Console.WriteLine(">> Test 4 successful");
+        }
 
-            test1();
+        static void test5()
+        {
+            String cleartext = "<.S.2K(Zq'";
+            String salt = "VYAclAMpaXY/oqAo9yUpku";
+            int cost = 4;
+            Bcrypt b = new Bcrypt(ref cleartext, ref salt, cost);
+            String expected_hash = "$2b$04$VYAclAMpaXY/oqAo9yUpkuWmoYywaPzyhu56HxXpVltnBIfmO9tgu";
+            String actual_hash = b.Hash;
+            assert(String.Equals(expected_hash, actual_hash), 5);
+            Console.WriteLine(">> Test 5 successful");
+        }
 
-            //////////////////////////////////////////////////////////////////////////////////////////////
+        static void test6()
+        {
+            String cleartext = "ggJ\\KbTnDG";
+            String salt = "$2b$07$4H896R09bzjhapgCPS/LYu";
+            Bcrypt b = new Bcrypt(ref cleartext, ref salt);
+            String expected_hash = "$2b$07$4H896R09bzjhapgCPS/LYuMzAQluVgR5iu/ALF8L8Aln6lzzYXwbq";
+            String actual_hash = b.Hash;
+            assert(String.Equals(expected_hash, actual_hash), 6);
+            Console.WriteLine(">> Test 6 successful");
+        }
+        static void Main(string[] args)
+        { 
+            bool all = true;
+            bool t1  = true;
+            bool t2  = true;
+            bool t3  = true;
+            bool t4  = true;
+            bool t5  = true;
+            bool t6  = true;
 
-            test2();
-
-            //////////////////////////////////////////////////////////////////////////////////////////////
-
-            Console.WriteLine("Test Case #3");
-            cleartext = "<.S.2K(Zq'";
-            salt = "VYAclAMpaXY/oqAo9yUpku";
-            cost = 4;
-            Bcrypt b3 = new Bcrypt(ref cleartext, ref salt, cost);
-            expected_hash = "$2a$04$VYAclAMpaXY/oqAo9yUpkuWmoYywaPzyhu56HxXpVltnBIfmO9tgu";
-            actual_hash = b3.Hash;
-            Console.WriteLine("Expected -> " + expected_hash);
-            Console.WriteLine("Actual   -> " + actual_hash);
-            Console.WriteLine("========================================================================");
-
-            //////////////////////////////////////////////////////////////////////////////////////////////
-
-            Console.WriteLine("Test Case #4");
-            cleartext = "<.S.2K(Zq'";
-            salt = "VYAclAMpaXY/oqAo9yUpku";
-            cost = 4;
-            Bcrypt b4 = new Bcrypt(ref cleartext, ref salt, cost);
-            expected_hash = "$2a$04$VYAclAMpaXY/oqAo9yUpkuWmoYywaPzyhu56HxXpVltnBIfmO9tgu";
-            actual_hash = b4.Hash;
-            Console.WriteLine("Expected -> " + expected_hash);
-            Console.WriteLine("Actual   -> " + actual_hash);
-            Console.WriteLine("========================================================================");
-
-            //////////////////////////////////////////////////////////////////////////////////////////////
-
-            Console.WriteLine("Test Case #5");
-            cleartext = "<.S.2K(Zq'";
-            salt = "VYAclAMpaXY/oqAo9yUpku";
-            cost = 4;
-            Bcrypt b5 = new Bcrypt(ref cleartext, ref salt, cost);
-            expected_hash = "$2a$04$VYAclAMpaXY/oqAo9yUpkuWmoYywaPzyhu56HxXpVltnBIfmO9tgu";
-            actual_hash = b5.Hash;
-            Console.WriteLine("Expected -> " + expected_hash);
-            Console.WriteLine("Actual   -> " + actual_hash);
-            Console.WriteLine("========================================================================");
-
-            //////////////////////////////////////////////////////////////////////////////////////////////
-            
-            Console.WriteLine("Test Case #6");
-            cleartext = "ggJ\\KbTnDG";
-            salt = "$2a$07$4H896R09bzjhapgCPS/LYu";
-            Bcrypt b6 = new Bcrypt(ref cleartext, ref salt);
-            expected_hash = "$2a$07$4H896R09bzjhapgCPS/LYuMzAQluVgR5iu/ALF8L8Aln6lzzYXwbq";
-            actual_hash = b6.Hash;
-            Console.WriteLine("Expected -> " + expected_hash);
-            Console.WriteLine("Actual   -> " + actual_hash);
-            Console.WriteLine("========================================================================");
-
+            if (t1 || all)
+            {
+                test1();
+            }
+            if (t2 || all)
+            {
+                test2();
+            }
+            if (t3 || all)
+            {
+                test3();
+            }
+            if (t4 || all)
+            {
+                test4();
+            }
+            if (t5 || all)
+            {
+                test5();
+            }
+            if (t6 || all)
+            {
+                test6();
+            }
         }
     }
 }
